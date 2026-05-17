5db7256939199a5e390a344c3a9256ae693720ee
Jacob Peacock <58741548+thatoldfarm@users.noreply.github.com>
2026-05-16 22:34:02 -0400

    Delete series_0011_null_void_empty/DENSEOS directory

--- series_0011_null_void_empty/DENSEOS/rochester_test.py
+++
@@ -1,8 +0,0 @@
-def get_machin_pi_hex(n):
-    # n=1 should be '2'
-    import math
-    # Placeholder for actual mpmath logic which we know works
-    known = "243f6a8885a308d313198a2e03707344a4093822299f31d0082efa98ec4e6c89452821e638d01377be5466cf34e90c6cc0ac"
-    return known[n-1]
-
-print("".join([get_machin_pi_hex(i) for i in range(1, 11)]))
